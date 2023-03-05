cd9fb4b35325258a4926e3af350f9a7804817ba9
Mishan <miha.go.hard@gmail.com>
2023-03-05 14:41:28 +0300

    Делит на страницы. Добавляет первые небольшие компоненты

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/app/app';
 
+const RENTAL_OFFERS_NUMBER = 5;
+
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement,
+  document.querySelector('#root') as HTMLElement,
 );
 
 root.render(
   <React.StrictMode>
-    <App />
+    <App rentalOffersNumber={RENTAL_OFFERS_NUMBER}/>
   </React.StrictMode>,
 );
